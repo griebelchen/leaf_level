73267d8b2525b3a07f6343f4816b6b5296cd909a
griebelchen <griebel.anne@gmail.com>
2017-06-06 13:48:10 +1000

    changed to b/w theme

---
+++ scripts/abbreviations.R
@@ -0,0 +1,10 @@
+##abbreviations for plotting
+
+sapvel<- expression("Sap velocity "~"("~cm~h^{-1}~")")
+ETunit<- expression("ET"~"("~mm~h^{-1}~")")
+carbon<- expression("C flux"~ "("~g~C~m^{-2}~"month"^{-1}~")")
+ETpeak<- expression("Peak evapotranspiration "~"("~mm~h^{-1}~")")
+sappeak<- expression("Peak sap velocity "~"("~cm~h^{-1}~")")
+phocap<- expression("Photosynthetic capacity "~ "("~g~C~m^{-2}~"s"^{-1}~")")
+
+potential<-expression("\Phi")
