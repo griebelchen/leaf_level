fb2fd7be859325b1de5535ecd1fca27d488e2876
griebelchen <griebel.anne@gmail.com>
2017-06-07 10:54:54 +1000

    layout changed to bw

--- scripts/traits.R
+++ scripts/traits.R
@@ -24,42 +24,48 @@ boxplot(water_potential~infestation,data=mol)
 
 par(mfrow=c(3,1))
 phi <- ggplot(data = df, aes(x=infestation, y=phi)) + ylim(-3.2,-1.4) +
-  geom_boxplot(aes(fill=infestation)) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none")
+  theme_bw() + geom_boxplot(aes()) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none")
  
 
 h2o <- ggplot(data = df, aes(x=infestation, y=h2o)) + ylim(0.45,0.75) + 
-  geom_boxplot(aes(fill=infestation)) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none")
+  theme_bw() + geom_boxplot(aes()) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none")
 
 lma <- ggplot(data = df, aes(x=infestation, y=lma)) + ylim(0.015,0.03) + 
-  geom_boxplot(aes(fill=infestation)) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none")
+  theme_bw() + geom_boxplot(aes()) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none")
 
 
 traits<-grid.arrange(phi, h2o , lma, nrow=3, ncol=1)
 ggsave("output/traits.png", plot = traits,  width = 20, height = 20, units = "cm", dpi = 300)
 
 phi_mis <- ggplot(data = df_mis, aes(x=infestation, y=phi)) + ylim(-3.2,-1.4) +
-  geom_boxplot(aes(fill=infestation)) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none") + labs(x="Mistletoe leaves")
+  theme_bw() + geom_boxplot(aes()) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none") + 
+  labs(x="Mistletoe leaves")
 
 
 h2o_mis <- ggplot(data = df_mis, aes(x=infestation, y=h2o)) + ylim(0.45,0.75) + 
-  geom_boxplot(aes(fill=infestation)) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none") + labs(x="Mistletoe leaves")
+  theme_bw() + geom_boxplot(aes()) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none") + 
+  labs(x="Mistletoe leaves")
 
 lma_mis <- ggplot(data = df_mis, aes(x=infestation, y=lma)) + ylim(0.015,0.03) + 
-  geom_boxplot(aes(fill=infestation)) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none") + labs(x="Mistletoe leaves")
+  theme_bw() + geom_boxplot(aes()) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none") + 
+  labs(x="Mistletoe leaves")
 
 
 traits_mis<-grid.arrange(phi_mis, h2o_mis , lma_mis, nrow=3, ncol=1)
 ggsave("output/traits_mis.png", plot = traits_mis,  width = 20, height = 20, units = "cm", dpi = 300)
 
 phi_euc <- ggplot(data = df_euc, aes(x=infestation, y=phi)) + ylim(-3.2,-1.4) +
-  geom_boxplot(aes(fill=infestation)) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none") + labs(x="Eucalypt leaves")
+  theme_bw() + geom_boxplot(aes()) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none") + 
+  labs(x="Eucalypt leaves")
 
 
 h2o_euc <- ggplot(data = df_euc, aes(x=infestation, y=h2o)) + ylim(0.45,0.75) + 
-  geom_boxplot(aes(fill=infestation)) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none") + labs(x="Eucalypt leaves")
+  theme_bw() + geom_boxplot(aes()) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none") + 
+  labs(x="Eucalypt leaves")
 
 lma_euc <- ggplot(data = df_euc, aes(x=infestation, y=lma)) + ylim(0.015,0.03) + 
-  geom_boxplot(aes(fill=infestation)) + facet_wrap( ~ species, scales="free") + theme(legend.position="none") + labs(x="Eucalypt leaves")
+  theme_bw() + geom_boxplot(aes()) + facet_wrap( ~ species, scales="free") + theme(legend.position="none") + 
+  labs(x="Eucalypt leaves")
 
 
 traits_euc<-grid.arrange(phi_euc, h2o_euc , lma_euc, nrow=3, ncol=1)
@@ -67,14 +73,17 @@ ggsave("output/traits_euc.png", plot = traits_euc,  width = 20, height = 20, uni
 
 
 phi_mel <- ggplot(data = mel, aes(x=infestation, y=phi)) + ylim(-3.2,-1.4) +
-  geom_boxplot(aes(fill=infestation)) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none") + labs(x="melaleuca leaves")
+  theme_bw() + geom_boxplot(aes()) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none") + 
+  labs(x="melaleuca leaves")
 
 
 h2o_mel <- ggplot(data = mel, aes(x=infestation, y=h2o)) + ylim(0.45,0.75) + 
-  geom_boxplot(aes(fill=infestation)) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none") + labs(x="melaleuca leaves")
+  theme_bw() + geom_boxplot(aes()) + facet_wrap( ~ species, scales="free")+ theme(legend.position="none") + 
+  labs(x="melaleuca leaves")
 
 lma_mel <- ggplot(data = mel, aes(x=infestation, y=lma)) + ylim(0.015,0.03) + 
-  geom_boxplot(aes(fill=infestation)) + facet_wrap( ~ species, scales="free") + theme(legend.position="none") + labs(x="melaleuca leaves")
+  theme_bw() + geom_boxplot(aes()) + facet_wrap( ~ species, scales="free") + theme(legend.position="none") + 
+  labs(x="melaleuca leaves")
 
 
 traits_mel<-grid.arrange(phi_mel, h2o_mel , lma_mel, nrow=3, ncol=1)
